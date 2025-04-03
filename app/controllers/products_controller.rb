3ab7d1d92a956555accf066459b3b34e695038d6
oscrosales <orosales2@academic.rrc.ca>
2025-04-03 18:33:09 -0500

    Added routes to product pages and created very basic index page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.includes(:brand)
+                      .find_by("id = ?", params[:id])
   end
 end
