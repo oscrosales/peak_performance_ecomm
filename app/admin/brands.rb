bb0ced9d08b30f9236f45bbba91f478c175d8827
oscrosales <orosales2@academic.rrc.ca>
2025-04-03 18:11:48 -0500

    Added file inputs for images to brands and products.

--- app/admin/brands.rb
+++ app/admin/brands.rb
@@ -1,5 +1,14 @@
 ActiveAdmin.register Brand do
-  permit_params :name, :description, :category_id
+  permit_params :name, :description, :category_id, :image
 
   remove_filter :image_attachment, :image_blob
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
