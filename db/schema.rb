8588ca4e5dcec3b618d1c9149513c56ab8e6becc
oscrosales <orosales2@academic.rrc.ca>
2025-04-10 16:57:32 -0500

    Generated User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_03_230119) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_10_215552) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -94,6 +94,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_03_230119) do
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.string "address"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
