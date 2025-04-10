78017db3263c0f48fc9e35bde9e698594964bd93
oscrosales <orosales2@academic.rrc.ca>
2025-04-10 16:59:40 -0500

    Generated User_Role joiner model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_10_215552) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_10_215913) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -98,6 +98,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_10_215552) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_roles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "role_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_user_roles_on_role_id"
+    t.index ["user_id"], name: "index_user_roles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -113,4 +122,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_10_215552) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "brands", "categories"
   add_foreign_key "products", "brands"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
