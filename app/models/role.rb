3841f9becd93aa50e035c727a1132d2e251154f7
oscrosales <orosales2@academic.rrc.ca>
2025-04-10 17:04:44 -0500

    Added validations and associations to users and roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,7 @@
 class Role < ApplicationRecord
+  has_many :user_roles
+  has_many :users, through: :user_roles
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
+  validates :name, :username, :password, :email, :address, :phone, presence: true
+  validates :name, :username, :email, :phone, uniqueness: true
 end
