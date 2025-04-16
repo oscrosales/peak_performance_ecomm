d1880e979ad5850a3ea9a5fc63dde5c93af7ba84
oscrosales <orosales2@academic.rrc.ca>
2025-04-16 15:25:26 -0500

    Fixed active admin issue

--- app/admin/brands.rb
+++ app/admin/brands.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register Brand do
   permit_params :name, :description, :category_id, :image
 
   remove_filter :image_attachment, :image_blob
+  config.filters = false
 
   form do |f|
     f.semantic_errors
