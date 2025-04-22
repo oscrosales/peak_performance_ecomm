d4f331ca2bdc9a48db23732d5283108942380d7e
oscrosales <orosales2@academic.rrc.ca>
2025-04-22 12:50:54 -0500

    Added validations to Provinces and seeded.

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,7 @@
 class Province < ApplicationRecord
+  has_many :users
+
+  validates :name, :abbreviation, :pst, presence: true
+  validates :name, :abbreviation, uniqueness: true
+  validates :pst, numericality: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,32 @@
-AdminUser.create!(
-    email: 'admin@example.com',
-    password: 'password',
-    password_confirmation: 'password'
-) if Rails.env.development?
+Province.destroy_all
+
+Provinces = [
+  { name: "Alberta", abbreviation: "AB", pst: 0.05 },
+  { name: "British Columbia", abbreviation: "BC", pst: 0.07 },
+  { name: "Manitoba", abbreviation: "MB", pst: 0.07 },
+  { name: "New Brunswick", abbreviation: "NB", pst: 0.10 },
+  { name: "Newfoundland and Labrador", abbreviation: "NL", pst: 0.10 },
+  { name: "Nova Scotia", abbreviation: "NS", pst: 0.10 },
+  { name: "Ontario", abbreviation: "ON", pst: 0.13 },
+  { name: "Prince Edward Island", abbreviation: "PE", pst: 0.10 },
+  { name: "Quebec", abbreviation: "QC", pst: 0.09975 },
+  { name: "Saskatchewan", abbreviation: "SK", pst: 0.06 },
+  { name: "Northwest Territories", abbreviation: "NT", pst: 0.05 },
+  { name: "Nunavut", abbreviation: "NU", pst: 0.05 },
+  { name: "Yukon", abbreviation: "YT", pst: 0.05 }
+]
+
+Provinces.each do |province|
+  Province.create!(province)
+end
+
+
+Provinces.each do |province|
+  Province.create!(province)
+end
+
+# AdminUser.create!(
+#     email: 'admin@example.com',
+#     password: 'password',
+#     password_confirmation: 'password'
+# ) if Rails.env.development?
