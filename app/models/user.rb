ca350bb84dfe1955622f50f1e36e81fe90bce16b
oscrosales <orosales2@academic.rrc.ca>
2025-04-22 12:51:09 -0500

    Added associations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  belongs_to :province
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
