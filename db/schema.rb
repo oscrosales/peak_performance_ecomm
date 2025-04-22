2306fab0b4a016382bc23a5b282ccee4a1200aea
oscrosales <orosales2@academic.rrc.ca>
2025-04-22 11:41:50 -0500

    Deleted existing users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_10_215913) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_22_163923) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -105,17 +105,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_10_215913) do
     t.datetime "updated_at", null: false
     t.index ["role_id"], name: "index_user_roles_on_role_id"
     t.index ["user_id"], name: "index_user_roles_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "password"
-    t.string "email"
-    t.string "address"
-    t.string "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
