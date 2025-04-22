a6c586005d921bb5ed4263e253aeb741da6a4222
oscrosales <orosales2@academic.rrc.ca>
2025-04-22 11:53:22 -0500

    Added custom fields to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :initialize_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :cart
   helper_method :categories
   helper_method :product_in_cart?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :name, :username, :address, :phone, :email, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+  end
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :username, :address, :phone, presence: true
+  validates :username, uniqueness: true
 end
