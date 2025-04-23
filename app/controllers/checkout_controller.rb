26aac687c52cec2550789078801e60c4b9c60269
oscrosales <orosales2@academic.rrc.ca>
2025-04-23 15:01:57 -0500

    Implemented Stripe for checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,29 @@
 class CheckoutController < ApplicationController
   def create
+    line_items = session[:cart].map do | product_id, quantity |
+      product = Product.find(product_id)
+      {
+        price_data: {
+          currency: "cad",
+          product_data: {
+            name: product.name,
+            description: product.description
+          },
+          unit_amount: (product.price * 100).to_i
+        },
+        quantity: quantity
+      }
+    end
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: [ "card" ],
+      success_url: checkout_success_url,
+      cancel_url: checkout_cancel_url,
+      mode: "payment",
+      line_items: line_items,
+    )
+
+    redirect_to session.url, allow_other_host: true
   end
 
   def success
