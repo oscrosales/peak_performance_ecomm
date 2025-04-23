071e5dc72768365de0453f45be969f12d7876d3f
oscrosales <orosales2@academic.rrc.ca>
2025-04-23 14:39:34 -0500

    Generated checkout controller and routed

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,12 @@ Rails.application.routes.draw do
     patch "update_quantity", on: :member
   end
 
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
